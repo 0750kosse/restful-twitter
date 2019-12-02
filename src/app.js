91658994b3511d4c3ef81b77dfff0410f083a7c8
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2019-12-02 09:01:45 +0000

    Basic functionality ok & initial mobile styling

--- src/app.js
+++ src/app.js
@@ -10,26 +10,28 @@ const methodOverride = require('method-override')
 //set up express app
 const app = express();
 //connect to mongodb
-mongoose.connect('mongodb://localhost/tweetapp')
+mongoose.connect('mongodb://localhost/tweetapp', { useNewUrlParser: true })
 mongoose.Promise = global.Promise;
 
 const handlebars = exphbs.create({
   layoutsDir: path.join(__dirname, "views/layouts"),
   partialsDir: path.join(__dirname, "views/partials"),
   defaultLayout: 'main',
-  extname: 'handlebars'
+  extname: 'handlebars',
 });
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
-const port = 3000;
+const port = 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(methodOverride('_method'));
+
+const m = moment().format('LLL');
 
 // GET resources
 app.get('/', (req, res) => {
@@ -41,7 +43,7 @@ app.get('/', (req, res) => {
 // POST to create new resource
 app.post('/', (req, res, next) => {
   Tweet.create(req.body).then(function (tweet) {
-    res.status(500).send(tweet)
+    res.send(tweet)
   }).catch(next)
   res.redirect('/')
 });
@@ -51,16 +53,18 @@ app.put('/:id', (req, res, next) => res.send({ type: 'PUT' }));
 
 // DELETE to delete an existing resource with a given id
 app.delete('/:id*?', (req, res, next) => {
-
   Tweet.findByIdAndRemove({ _id: req.params.id }).then(function (tweet) {
-    res.status(500).send(tweet)
+    res.send(tweet)
   }).catch(next)
   res.redirect('/')
 });
 
 //error handling middleware
 app.use(function (err, req, res, next) {
-  res.status(422).send({ error: err.message })
+  res.status(500).send({ error: err.message })
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}`))
+
+
+
